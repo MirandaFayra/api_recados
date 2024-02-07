5879c418f0b559f4639f2d300338c1289e66aef3
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-02-07 16:41:27 -0300

    Finalizar endpoint de signup

--- src/index.js
+++ src/index.js
@@ -6,11 +6,65 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let users = []
+let menssage = []
+
+let nextUserId = 1 
+
+let nextMessageId = 1 
+
+//------- TEST----
 
 app.get('/',(req,res)=>{
     res.status(200).send('Welcome to this application');
 })
 
-app.listen(3000, ()=>{
+//------- SIGNUP----
+
+app.post('/signup',(req,res)=>{
+    const {name, email , password} = req.body
+
+    
+    if(!name){
+        res.status(400).json({
+            message: 'Please verify if you pass a valid name'
+        })
+    }
+
+    if(!email){
+        res.status(400).json({
+            message: 'Please verify if you pass a valid  email adress'
+        })
+    }
+
+    if(!password){
+        res.status(400).json({
+            message: 'Please verify if you pass a valid  password'
+        })
+    }
+ 
+    const newUser = {
+        id: nextUserId, 
+        name: name,
+        email: email,
+        password: password
+    }
+
+    users.push(newUser)
+
+    nextUserId ++
+
+    console.log(newUser)
+
+    res.status(201).json({
+        sucess: true,
+        message: ' User registered successfully'
+    })
+    
+}) 
+
+//------- VERIFY----
+
+app.listen(3333, ()=>{
     console.log("Server is running in http://localhost:3333")
 })
